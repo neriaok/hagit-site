616958642a8ea620e0152555a7f1ad49280f8b11
neriaok <neriaokabik.com>
2025-11-09 13:31:17 +0200

    server changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // לכל בקשה אחרת, להחזיר את index.html
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
